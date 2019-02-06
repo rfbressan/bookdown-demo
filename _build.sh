3243737a6fa349954845812914d8fb3828b4a646
Rafael Bressan <rfbressan@gmail.com>
2019-02-06 09:08:40 -0200

    before_install

--- _build.sh
+++ _build.sh
@@ -2,9 +2,6 @@
 
 set -ev
 
-# Installs system libraries needed for R packages
-sudo apt-get update && apt-get -y install "^libquantlib*"
-
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
